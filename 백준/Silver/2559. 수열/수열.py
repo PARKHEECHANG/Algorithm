ded8af7e490bb703b9c62101442ca096ebc726b4
PARKHEECHANG <122577719+PARKHEECHANG@users.noreply.github.com>
2023-03-06 00:12:48 +0900

    [Silver III] Title: 수열, Time: 92 ms, Memory: 40292 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,16 +1,13 @@
 n, k = map(int, input().split())
 lst = list(map(int, input().split()))
-
-p = 0
 sm = 0
-for i in range(p, p+k) :
+for i in range(k) :
     sm += lst[i]
 
 mx = sm
 for i in range(n-k) :
     sm -= lst[i]
     sm += lst[i+k]
-
     mx = max(sm, mx)
 
 print(mx)
